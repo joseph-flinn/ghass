b52a8418b0b42c68e4b9cd99affebac4b85fae48
Joseph Flinn <joseph.s.flinn@gmail.com>
2023-12-13 06:47:52 -0800

    setting up the express server. Having an issue with the Octokit authenticated context...

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
+const sodium = require('libsodium-wrappers')
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
-module.exports = (app) => {
+module.exports = (app, { getRouter }) => {
   // Your code here
-  app.log.info("Yay, the app was loaded!");
+  
+  const router = getRouter("/ghass");
+  router.use(require("express").static("public"));
+  router.use(require("express").json());
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
+
+  router.post("/octokit", async (req, res) => {
+    const octokit = await app.auth()
+    const { data } = await octokit.apps.getAuthenticated()
+
+    res.json(data)
   });
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  // UNSAFE!! Payload is not yet encrypted on POST. Need to encrypt it locally with Org Public Key
+  router.post("/dsync", async (req, res) => {
+    const octokit = await app.auth()
+    const payload = req.body
+
+    const org = "flinnsolutions"
+    const repo = "gha-secret-sync-test"
+    const repoId = ""
+    const env = "prod"
+
+    const data = {
+      "orgPublicKey": await octokit.rest.actions.getOrgPublicKey(org),
+      "repoPublicKey": await octokit.rest.actions.getRepoPublicKey(org, repo),
+      //"envPublicKey": await octokit.rest.actions.getEnvironmentPublicKey(repoId, env)
+    }
+
+    res.json(data);
+  });
+
+  app.log.info("Yay, the app was loaded!");
 };
