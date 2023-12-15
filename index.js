35b4dae34cafdc79712ff819241764ae6f33d819
Joseph Flinn <joseph.s.flinn@gmail.com>
2023-12-15 05:50:29 -0800

    Rough working auth + request! Add Taskfile for tracking all of the cURL parameters

--- index.js
+++ index.js
@@ -13,30 +13,41 @@ module.exports = (app, { getRouter }) => {
 
 
   router.post("/octokit", async (req, res) => {
-    const octokit = await app.auth()
-    const { data } = await octokit.apps.getAuthenticated()
+    try {
+      // Root installation ID is: 44652407
+      const octokit = await app.auth(44652407)
+      const { data } = await octokit.apps.getAuthenticated()
 
-    res.json(data)
+      res.json(data)
+    } catch (error) {
+      console.error(error)
+      res.json({"message": "/dsync failed"})
+    }
   });
 
 
   // UNSAFE!! Payload is not yet encrypted on POST. Need to encrypt it locally with Org Public Key
   router.post("/dsync", async (req, res) => {
-    const octokit = await app.auth()
-    const payload = req.body
+    try {
+      const octokit = await app.auth(44652407)
+      const payload = req.body
 
-    const org = "flinnsolutions"
-    const repo = "gha-secret-sync-test"
-    const repoId = ""
-    const env = "prod"
+      const owner = "flinnsolutions"
+      const repo = "gha-secret-sync-test"
+      const repoId = 726204636
+      const env = "prod"
 
-    const data = {
-      "orgPublicKey": await octokit.rest.actions.getOrgPublicKey(org),
-      "repoPublicKey": await octokit.rest.actions.getRepoPublicKey(org, repo),
-      //"envPublicKey": await octokit.rest.actions.getEnvironmentPublicKey(repoId, env)
+      const data = {
+        "orgPublicKey": await octokit.rest.actions.getOrgPublicKey({org: owner}),
+        "repoPublicKey": await octokit.rest.actions.getRepoPublicKey({owner: owner, repo: repo}),
+        "envPublicKey": await octokit.rest.actions.getEnvironmentPublicKey({repository_id: repoId, environment_name: env})
+      }
+
+      res.json(data);
+    } catch (error) {
+      console.error(error)
+      res.json({"message": "/dsync failed"})
     }
-
-    res.json(data);
   });
 
   app.log.info("Yay, the app was loaded!");
